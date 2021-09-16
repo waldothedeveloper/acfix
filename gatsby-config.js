ca021636ff933195f98f9c9169271dcd964208a5
Waldo Lavaut <waldothedeveloper@gmail.com>
2021-09-16 14:26:20 -0400

    Cleaned up the initial gatsby configuration and readme

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,9 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
-    siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
+    title: `AC Fix official websitegi`,
+    description: `AC at home broken? Look no more. We have the best prices. Period`,
+    author: `AC FIX TODAY`,
+    siteUrl: `https://www.acfix.today`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
